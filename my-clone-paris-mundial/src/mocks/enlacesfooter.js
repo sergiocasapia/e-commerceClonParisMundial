e42cf7c01ba971d7ed97edd11295da1dd0f0d5a1
sergiocasapia <sergio.casapia@gmail.com>
2026-06-15 00:08:16 -0400

    ajuste referencia link footer

--- my-clone-paris-mundial/src/mocks/enlacesfooter.js
+++ my-clone-paris-mundial/src/mocks/enlacesfooter.js
@@ -1,7 +1,7 @@
 export const enlacesFooter = [
-    { id: 1, texto: "Centro de Ayuda", url: "/ayuda" },
-    { id: 2, texto: "Mundo Paris", url: "/mundo" },
-    { id: 3, texto: "Cencosud", url: "/cenco" },
-    { id: 4, texto: "Medios de pago", url: "/pagos" },
-    { id: 5, texto: "Síguenos 🅾ⓕ𝕏", url: "/siguenos" }
+    { id: 1, texto: "Centro de Ayuda", url: "#" },
+    { id: 2, texto: "Mundo Paris", url: "#" },
+    { id: 3, texto: "Cencosud", url: "#" },
+    { id: 4, texto: "Medios de pago", url: "#" },
+    { id: 5, texto: "Síguenos 🅾ⓕ𝕏", url: "#" }
   ];
