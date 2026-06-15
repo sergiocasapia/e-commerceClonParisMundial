99d00d0722ab582a80b7a206af0a6f24d3eec562
sergiocasapia <sergio.casapia@gmail.com>
2026-06-14 23:07:12 -0400

    mover imagenes a public

--- my-clone-paris-mundial/src/mocks/articulos.js
+++ my-clone-paris-mundial/src/mocks/articulos.js
@@ -1,13 +1,13 @@
 
 export const articulos = [ 
-    { id: 1, marca: "Panini", categoria: "Juguete", descripcion: "Pack 10 Sobres Mundial 2026 Stickers Original", price: "$19.260 CLP", isSuperDespach: false, slug: "juguetes", imagen: "../src/assets/imagenes/1Pack10SobrePaniniMundial2026.jpg" },
-    { id: 2, marca: "TCL", categoria: "Tecnología", descripcion: "Mini LED Smart TV 65'' UHD 4K C6K Gamer", price: "$300.000 CLP", isSuperDespach: true, slug: "tecnologia", imagen: "../src/assets/imagenes/2MiniLDSmartTV54.jpg" },
-    { id: 3, marca: "Hasbro", categoria: "Juguete", descripcion: "Juego de Mesa Monopoly Panini Prizm: Copa Mundial", price: "$44.260 CLP", isSuperDespach: true, slug: "juguetes", imagen: "../src/assets/imagenes/3MonopolyMundial.jpg" },
-    { id: 4, marca: "Adidas", categoria: "Deportes", descripcion: "Balón de Fútbol Mundial 2026 Trionda Training en Caja", price: "$22.990 CLP", isSuperDespach: true, slug: "deportes", imagen: "../src/assets/imagenes/4BalonMundial.jpg" },
-    { id: 5, marca: "Adidas", categoria: "Moda", descripcion: "Camiseta de Fútbol Mundial Manga Corta de Niño", price: "$12.990 CLP", isSuperDespach: false, slug: "moda", imagen: "../src/assets/imagenes/5CamisetaArgentina.jpg" },
-    { id: 6, marca: "Note", categoria: "Electro", descripcion: "Celular Note 5G Promocion Datos Gratis", price: "$150.000 CLP", isSuperDespach: false, slug: "electro", imagen: "../src/assets/imagenes/6CelularMundial.jpg" },
-    { id: 7, marca: "Mabe", categoria: "Hogar", descripcion: "Refriguerador Mabe Promocion un Pack Bebidas", price: "$259.000 CLP", isSuperDespach: true, slug: "hogar", imagen: "../src/assets/imagenes/7RefrigeradorMundia.jpg" },
-    { id: 8, marca: "Lego", categoria: "Juguete", descripcion: "Lego Lionel Messi: Leyenda del Fútbol Mundial", price: "$129.000 CLP", isSuperDespach: true, slug: "juguetes", imagen: "../src/assets/imagenes/8LegoMessi.jpg" },
-    { id: 9, marca: "Hasbro", categoria: "Juguete", descripcion: "Trio de Mascota Mundial", price: "$90.000 CLP", isSuperDespach: false, slug: "juguetes", imagen: "../src/assets/imagenes/9MascotasMundial.jpg" },
-    { id: 10, marca: "Panini", categoria: "Hogar", descripcion: "Tazones Leyenda del Fútbol Mundial", price: "$35.000 CLP", isSuperDespach: false, slug: "hogar", imagen: "../src/assets/imagenes/10TazonesMundial.jpg" },
+    { id: 1, marca: "Panini", categoria: "Juguete", descripcion: "Pack 10 Sobres Mundial 2026 Stickers Original", price: "$19.260 CLP", isSuperDespach: false, slug: "juguetes", imagen: "/e-commerceClonParisMundial/imagenes/1Pack10SobrePaniniMundial2026.jpg" },
+    { id: 2, marca: "TCL", categoria: "Tecnología", descripcion: "Mini LED Smart TV 65'' UHD 4K C6K Gamer", price: "$300.000 CLP", isSuperDespach: true, slug: "tecnologia", imagen: "/e-commerceClonParisMundial/imagenes/2MiniLDSmartTV54.jpg" },
+    { id: 3, marca: "Hasbro", categoria: "Juguete", descripcion: "Juego de Mesa Monopoly Panini Prizm: Copa Mundial", price: "$44.260 CLP", isSuperDespach: true, slug: "juguetes", imagen: "/e-commerceClonParisMundial/imagenes/3MonopolyMundial.jpg" },
+    { id: 4, marca: "Adidas", categoria: "Deportes", descripcion: "Balón de Fútbol Mundial 2026 Trionda Training en Caja", price: "$22.990 CLP", isSuperDespach: true, slug: "deportes", imagen: "/e-commerceClonParisMundial/imagenes/4BalonMundial.jpg" },
+    { id: 5, marca: "Adidas", categoria: "Moda", descripcion: "Camiseta de Fútbol Mundial Manga Corta de Niño", price: "$12.990 CLP", isSuperDespach: false, slug: "moda", imagen: "/e-commerceClonParisMundial/imagenes/5CamisetaArgentina.jpg" },
+    { id: 6, marca: "Note", categoria: "Electro", descripcion: "Celular Note 5G Promocion Datos Gratis", price: "$150.000 CLP", isSuperDespach: false, slug: "electro", imagen: "/e-commerceClonParisMundial/imagenes/6CelularMundial.jpg" },
+    { id: 7, marca: "Mabe", categoria: "Hogar", descripcion: "Refriguerador Mabe Promocion un Pack Bebidas", price: "$259.000 CLP", isSuperDespach: true, slug: "hogar", imagen: "/e-commerceClonParisMundial/imagenes/7RefrigeradorMundia.jpg" },
+    { id: 8, marca: "Lego", categoria: "Juguete", descripcion: "Lego Lionel Messi: Leyenda del Fútbol Mundial", price: "$129.000 CLP", isSuperDespach: true, slug: "juguetes", imagen: "/e-commerceClonParisMundial/imagenes/8LegoMessi.jpg" },
+    { id: 9, marca: "Hasbro", categoria: "Juguete", descripcion: "Trio de Mascota Mundial", price: "$90.000 CLP", isSuperDespach: false, slug: "juguetes", imagen: "/e-commerceClonParisMundial/imagenes/9MascotasMundial.jpg" },
+    { id: 10, marca: "Panini", categoria: "Hogar", descripcion: "Tazones Leyenda del Fútbol Mundial", price: "$35.000 CLP", isSuperDespach: false, slug: "hogar", imagen: "/e-commerceClonParisMundial/imagenes/10TazonesMundial.jpg" },
 ];
