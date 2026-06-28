08088789d2fb5e58578a42b8aab24225ac790a1c
sergiocasapia <sergio.casapia@gmail.com>
2026-06-28 00:43:52 -0400

    Card commit : Se incorpora styles a CSS module de presentacion de productos

--- my-clone-paris-mundial/src/components/ProductCardComponent/productCard.jsx
+++ my-clone-paris-mundial/src/components/ProductCardComponent/productCard.jsx
@@ -14,7 +14,7 @@ function ProductCard({ articulo }) {
         <p className={styles['product-marca']}>{articulo.title}</p>
         <p className={styles['product-descripcion']}>{articulo.description}</p>
         <p className={styles['product-precio']}>{articulo.price}</p>
-        { articulo.isSuperDespach ? <p className={styles['product-despacho']}>⚽Despacho Gratis🏆</p> : <p></p>}
+        { articulo.price >= 200.00 ? <p className={styles['product-despacho']}>⚽Despacho Gratis🏆</p> : <p></p> }
         <Button productcarro titulo="🛒 Agregar al carro" onClick={agregaCarro} />
       </div>
       </>
