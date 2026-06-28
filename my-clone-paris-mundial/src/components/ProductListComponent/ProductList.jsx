05d5123fc4fa0ce685634d6d8d125955983f9258
sergiocasapia <sergio.casapia@gmail.com>
2026-06-28 00:26:01 -0400

    CardList commit : Se modifica incorporando conexion a Api y se mod a CSS Module

--- my-clone-paris-mundial/src/components/ProductListComponent/ProductList.jsx
+++ my-clone-paris-mundial/src/components/ProductListComponent/ProductList.jsx
@@ -1,14 +1,38 @@
-import '../ProductListComponent/ProductList.css'
+import styles from '../ProductListComponent/ProductList.module.css'
 import ProductCard from '../ProductCardComponent/ProductCard.jsx'
 
+import { useEffect } from "react";
+import { useState } from "react";
+
 function ProductList({ articulos }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect ( () => {
+    fetch('https://dummyjson.com/products')
+    .then((res) => res.json())
+    .then((data) =>{
+        setProducts(data.products)
+    })
+    .catch((err) => {
+        setError(err.message)
+    })
+    .finally(() => {
+        setLoading(false)
+    })
+  }, []
+)
+
+if (loading) return <p>Cargando Productos</p>
+if (error) return <p>Hubo un Error</p>
+
   return (
-    <div className="product-list">
-      {articulos.map((articulo) => (
-        <ProductCard key={articulo.id} articulo={articulo} />
-      ))}
+    <div className={styles['product-list']}>
+      {products.map((product) => (<ProductCard key={product.id} articulo={product}/>))}
     </div>
   );
+
 }
 
 export default ProductList;
